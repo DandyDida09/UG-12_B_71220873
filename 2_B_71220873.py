6e215a68136fdd686450ab993605ae7502f417e3
DandyDida09 <117884467+DandyDida09@users.noreply.github.com>
2022-12-08 12:58:05 +0700

    Add files via upload

---
+++ 2_B_71220873.py
@@ -0,0 +1,18 @@
+#penjumlahan
+print(1 + 3)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak #
+print(hasil)
+
+print(2 + 2)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak #
+print(hasil)
+
+print(3 + 1)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak #
+print(hasil)
